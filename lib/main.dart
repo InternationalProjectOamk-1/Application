4b6a0c851f5cfbd2697a811b0706a13c3020e02a
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-03-21 15:36:49 +0200

    Changed file name to more suitable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mapplication/loginScreen.dart';
+import 'package:mapplication/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen()
+      home: const LoginScreen()
 
     );
   }
