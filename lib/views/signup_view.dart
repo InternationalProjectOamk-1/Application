a302cd31a4584053d9305c93f956075b21b89da7
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-03-21 15:41:23 +0200

    Created signup_view with added input fields

---
+++ lib/views/signup_view.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../widgets/input_field_widget.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({ Key? key }) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwController1 = TextEditingController();
+  TextEditingController pwController2 = TextEditingController();
+
+  void forgor() {
+  print("Forgot password pressed");
+  }
+
+  void signUp() {
+  print("Sign Up pressed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff5ac18e),
+                    Color(0xff5ac18e),
+                    Color(0xff5ac18e),
+                    Color(0xff5ac18e),
+                    Color(0xff5ac18e),
+                  ]
+                )
+              ),
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 120,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:  <Widget>[
+                    const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'Mont',
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    buildFields(
+                      controllerType: emailController,
+                      text:'Email', 
+                      type: TextInputType.emailAddress, 
+                      hintText: 'Email', 
+                      iconType: Icons.email,
+                      obscure: false,
+                    ),
+                    const SizedBox(height: 10),
+                    buildFields(
+                      controllerType: pwController1,
+                      text:'Password', 
+                      type: TextInputType.text, 
+                      hintText: 'Password', 
+                      iconType: Icons.lock,
+                      obscure: true
+                    ),
+                    const SizedBox(height: 10), //TO-DO: Add password confirmation function
+                    buildFields(
+                      controllerType: pwController2,
+                      text:'Confirm Password', 
+                      type: TextInputType.text, 
+                      hintText: 'Password', 
+                      iconType: Icons.lock,
+                      obscure: true
+                    ),
+                    const SizedBox(height: 50),
+                    Container(
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: TextButton(
+                        onPressed: () => print('email: ${emailController.text}\npassword: ${pwController1.text}'),
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontFamily: 'Mont',
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),  
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
