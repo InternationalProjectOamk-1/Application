6e8bdbdae87abc7a2365cab0c47e10adfc293ced
Andy <pmduck@hotmail.com>
2022-03-23 13:36:48 +0200

    feed list works with dummy db without update function

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mapplication/data/events.dart';
 import 'package:mapplication/widgets/bottom_bar.dart';
+import 'package:mapplication/widgets/event_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  static const data = {
-    "id": 1,
-    "description": "City",
-    "interests": [3],
-    "members": [4],
-    "title": "New York",
-    "locationBased": true,
-    "latitude": 40.7127281,
-    "longitude": -74.0060152,
-    "hostID": 9,
-    "maxPeople": 10,
-    "minPeople": 2,
-    "startEvent": "2022-03-22T08:39:02.619Z",
-    "hasStarted": true
-  };
 
   @override
   Widget build(BuildContext context) {
@@ -28,34 +14,11 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Feeds",
-                    style: Theme.of(context).textTheme.displayMedium,
-                  ),
-                ),
-                Expanded(
-                  child: GridView.count(
-                    crossAxisCount: 1,
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.orange[100],
-                          borderRadius: BorderRadius.circular(13),
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Text(data["title"].toString()),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+            child: ListView.builder(
+              itemCount: event_data.length,
+              itemBuilder: (context, index) {
+                return EventBuilder(context, index);
+              },
             ),
           )
         ],
