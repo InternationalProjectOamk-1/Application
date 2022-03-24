44d1523520ed69daad3d458b5260e70985f49460
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-03-24 14:02:45 +0200

    Changed home to LoginScreen() for working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mapplication/home_screen.dart';
+import 'package:mapplication/views/login_view.dart';
 
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen()
+      home: const LoginScreen()
     );
   }
 }
