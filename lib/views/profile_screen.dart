cad7385eaf9766f92b5b1ccd6bf481fa3951e8ec
Andy <pmduck@hotmail.com>
2022-03-28 21:16:18 +0300

    profile with default circle profile pic

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mapplication/widgets/profile_button.dart';
 
@@ -6,9 +7,38 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    File? image;
+
     return Scaffold(
+      backgroundColor: Colors.orange[100],
       body: Stack(
         children: <Widget>[
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => {},
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: SizedBox.square(
+                        dimension: size.width * .5,
+                        child:
+                            image != null ? Image.file(image) : FlutterLogo(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           ProfileButton(
             icon: Icons.keyboard_return,
             press: () => Navigator.pop(context),
