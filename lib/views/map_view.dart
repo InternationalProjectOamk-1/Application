03a40290c49aacb1ca4e8a239b0cfc52826e7059
Aleksi <79052109+Haxeli@users.noreply.github.com>
2022-04-12 18:49:26 +0300

    Add a new marker
    LongPress now adds a draggable marker

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -10,29 +10,47 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  List<Marker> events = [];
   final userPos = const LatLng(65.0135579, 25.4809041);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ActMap'),
-      ),
       body: Stack(
         children: <Widget>[
           GoogleMap(
+            zoomControlsEnabled: false,
+            rotateGesturesEnabled: true,
             initialCameraPosition: CameraPosition(
               target: userPos,
               zoom: 12,
             ),
+            markers: Set.from(events),
+            onLongPress: _handleLongPress,
           ),
           Container(
-            alignment: Alignment.bottomCenter,
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+            alignment: Alignment.bottomLeft,
+            padding: const EdgeInsets.fromLTRB(70, 0, 0, 8),
             child: const Text('ActMap v. 0.1'),
           )
         ],
       ),
     );
+  }
+
+  _handleLongPress(LatLng pos) {
+    setState(() {
+      events = [];
+      events.add(Marker(
+        markerId: MarkerId(pos.toString()),
+        position: pos,
+        draggable: true,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        infoWindow: const InfoWindow(
+          title: 'My event',
+          snippet: 'Event taking place',
+        ),
+      ));
+    });
   }
 }
