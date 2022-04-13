08b40f9d8f0a53f5d755cac8b3a424bfb00839e9
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-04-13 18:42:57 +0300

    Widget and body for Messaging screen

---
+++ lib/widgets/Chat/chat_messaging_body.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mapplication/consts/styling_consts.dart';
+
+class ChatScreenBody extends StatelessWidget {
+  const ChatScreenBody({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Container(
+          height: 50,
+          decoration:  BoxDecoration(
+            color: Colors.white.withOpacity(0.6),
+          ),
+          child: SafeArea(
+            child: Row(
+              children: [
+                const SizedBox(width: appDefaultPadding * 2),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: appDefaultPadding 
+                    ),
+                    decoration: BoxDecoration(
+                      color: appDefaultColor.withOpacity(0.10), 
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Row(
+                      children: [
+                        const SizedBox(width: appDefaultPadding / 2),
+                        const Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Type here...',
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: (){},
+                          icon: const Icon(Icons.send_rounded)
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: appDefaultPadding * 2),
+              ],
+            ),
+          ),
+        )
+      ],
+      
+    );
+  }
+}
