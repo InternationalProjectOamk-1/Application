f02f1566939c3ed9494f4e693ece0363f1308dce
v0peak00 <v0peak00@students.oamk.fi>
2022-04-19 15:13:40 +0300

    double_back_to_close_app import and removed unused

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:mapplication/views/chat_view.dart';
 import 'package:mapplication/views/home_screen.dart';
 import 'package:mapplication/views/login_view.dart';
 import 'package:mapplication/views/map_view.dart';
 import 'package:mapplication/widgets/bottom_bar.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 
 
 void main() {
@@ -27,15 +28,23 @@ class MyApp extends StatelessWidget {
         bottomNavigationBar: BottomBar(
           controller: controller,
         ),
-        body: PageView(
-          onPageChanged: (controller) => print(controller),
-          controller: controller,
-          scrollDirection: Axis.horizontal,
-          children: const <Widget>[
-            LoginScreen(),
-            HomeScreen(),
-            MapScreen(),
-          ],
+        body: DoubleBackToCloseApp(
+          snackBar: const SnackBar(
+            content: Text(
+              'Tap again to exit',
+              ),
+          ),
+          child: PageView(
+            onPageChanged: (controller) => print(controller),
+            controller: controller,
+            scrollDirection: Axis.horizontal,
+            children: const <Widget>[
+              ChatScreen(),
+              LoginScreen(),
+              HomeScreen(),
+              MapScreen(),
+            ],
+          ),
         ),
       ),
     );
