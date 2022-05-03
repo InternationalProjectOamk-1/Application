a34b5752216d36eb8c98bd812299620081b281e1
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-05-03 17:13:34 +0300

    Added: - new constants

---
+++ lib/consts/service_consts.dart
@@ -0,0 +1,12 @@
+const String BASE_PATH = 'office.pepr.com:25252';
+
+const String LOGIN= '/Login';
+
+const String REGISTER = 'Login/Register';
+
+
+// Different url for chat service
+const String CHAT = 'http://office.pepr.com:25253/chat';
+
+const String CHAT_ALL_MESSAGES = 'office.pepr.com:5100/ChatHistory/Get/';
+
