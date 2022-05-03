58fa8732f453c67939957de91fccbffd023defad
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-05-03 17:15:55 +0300

    TO BE DELETED - message.dart - user_service_consts.dart

--- lib/components/Chat/message.dart
+++ lib/components/Chat/message.dart
@@ -13,6 +13,7 @@ class Message extends StatelessWidget {
 
   final ChatMessage message;
   final Chat chat;
+  final bool isSender = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,59 +21,19 @@ class Message extends StatelessWidget {
       padding: const EdgeInsets.only(top: appDefaultPadding),
       child: Row(
         mainAxisAlignment: 
-          message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+          isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
-            if (!message.isSender) ...[
+            if (!isSender) ...[
               CircleAvatar(
                 radius: 12,
                 backgroundImage: AssetImage(chat.image),
               ),
               const SizedBox(width: appDefaultPadding / 1.5,)
             ],
-            TextMessage(message: message),
-            if (message.isSender) MessageStatusDot(status: message.messageStatus,)
+            TextMessage(chatMessage: message),
           ],
       ),
     );
   }
 }
 
-class MessageStatusDot extends StatelessWidget {
-  const MessageStatusDot({
-      Key? key,
-      required this.status,
-  }) : super(key: key);
-
-  final MessageStatus status;
-
-  @override
-  Widget build(BuildContext context) {
-
-    Color statusColor(MessageStatus status) {
-      switch (status) {
-        case MessageStatus.not_sent:
-          return errorColor;
-        case MessageStatus.not_viewed:
-          return appDefaultColor.withOpacity(0.1);
-        case MessageStatus.viewed:
-          return appDefaultColor.withOpacity(0.6);
-        default:
-          return Colors.transparent;
-      }
-    }
-
-    return Container(
-      margin: const EdgeInsets.only(left: appDefaultPadding / 2),
-      height: 12,
-      width: 12,
-      decoration: BoxDecoration(
-        color: statusColor(status),
-        shape: BoxShape.circle,
-      ),
-      child: Icon(
-        status == MessageStatus.not_sent ? Icons.close : Icons.check,
-        size: 10,
-        ),
-    );
-  }
-}
--- lib/consts/user_service_consts.dart
+++
@@ -1,6 +0,0 @@
-const String BASE_PATH = 'office.pepr.com:25252';
-
-const String LOGIN= '/Login';
-
-const String REGISTER = 'Login/Register';
-
