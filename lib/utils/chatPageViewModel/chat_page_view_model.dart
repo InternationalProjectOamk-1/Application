09fbcc677a875cff8c92ee6c8efbffcb59e81ad6
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-05-09 18:17:46 +0200

    Added: - ChatMessage class - GetAllMessages func

--- lib/utils/chatPageViewModel/chat_page_view_model.dart
+++ lib/utils/chatPageViewModel/chat_page_view_model.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mapplication/consts/service_consts.dart' as consts;
 import 'package:signalr_netcore/signalr_client.dart';
@@ -7,25 +8,22 @@ import 'package:mapplication/utils/viewModel/view_model_provider.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
 
+
 typedef HubConnectionProvider = Future<HubConnection> Function();
 
-class ChatMessage {
-  // Properites
-  final String user;
-  final String message;
 
-  // Methods
-  ChatMessage(this.user, this.message);
-}
+
 
 class ChatPageViewModel extends ViewModel {
-  // Properties
+
+  // Properties start
+  
   late String _serverUrl;
   late HubConnection _hubConnection;
   HubConnectionState hubState = HubConnectionState.Disconnected;
   late Logger _logger;
   late StreamSubscription<LogRecord> _logMessagesSub;
-
+ 
   late List<ChatMessage> _chatMessages;
   static const String chatMessagesPropName = "chatMessages";
   List<ChatMessage> get chatMessages => _chatMessages;
@@ -40,20 +38,29 @@ class ChatPageViewModel extends ViewModel {
     updateValue(roomPropName, _room, value, (v) => _room = value);
   }
 
-  // Methods
+  late String _username;
+  static const String usernamePropName = "username";
+  String get username => _username;
+  set setUsername(String value) {
+    updateValue(usernamePropName, _username, value, (v) => _username = value);
+  }
+
+  // Methods start
+
   ChatPageViewModel() {
+    
     _serverUrl = consts.CHAT;
     _chatMessages = [];
     _room = "general";
     _hubConnection = HubConnectionBuilder()
-          .withUrl(_serverUrl)
-          .withAutomaticReconnect(retryDelays: [2000, 5000, 10000, 20000])
-          .build();
+      .withUrl(_serverUrl)
+      .withAutomaticReconnect(retryDelays: [2000, 5000, 10000, 20000])
+      .build();
     
     Logger.root.level = Level.ALL;
     _logMessagesSub = Logger.root.onRecord.listen(_handleLogMessage);
     _logger = Logger("ChatPageViewModel");
-
+    
     openChatConnection();
   }
 
@@ -67,7 +74,6 @@ class ChatPageViewModel extends ViewModel {
     print(msg.message);
   }
 
-
   Future<void> openChatConnection() async {
     final logger = _logger;
 
@@ -80,13 +86,12 @@ class ChatPageViewModel extends ViewModel {
       );
     if (hubState == HubConnectionState.Disconnected) {
       _hubConnection = HubConnectionBuilder()
-          .withUrl(_serverUrl, options: httpConnectionOptions)
-          .withAutomaticReconnect(retryDelays: [2000, 5000, 10000, 20000])
-          //.configureLogging(logger)
-          .build();
-    }
+        .withUrl(_serverUrl, options: httpConnectionOptions)
+        .withAutomaticReconnect(retryDelays: [2000, 5000, 10000, 20000])
+        //.configureLogging(logger)
+        .build();
+      }
       _hubConnection.onclose(({error}) => print("Connection closed"));
-      
       _hubConnection.on("receiveMessage", receiveMessage);
     }
 
@@ -95,70 +100,70 @@ class ChatPageViewModel extends ViewModel {
     }
   }
 
-  
-  // SendMessage(string user, string message, string room, bool join, bool leave)
-  Future<void> sendChatMessage(String chatMessage, String room) async {
-    if (chatMessage == null || chatMessage.length == 0) {
+  Future<void> sendChatMessage(String chatMessage, String room, String username) async {
+    if (chatMessage.isEmpty || room.isEmpty) {
       return;
     }
-    _hubConnection.invoke("SendMessage", args: <Object>["Username", chatMessage, _room, false, false]);
+    _hubConnection.invoke("SendMessage", args: <Object>[username, chatMessage, _room, false, false]);
   }
 
-  void join(String room) async{
-     if (room == null || room.length == 0) {
+  void join(String room, String username) async{
+    _chatMessages.clear();
+    if (room.isEmpty) {
       return;
     }
-    _chatMessages.clear();
-    notifyPropertyChanged(chatMessagesPropName);
-    _hubConnection.invoke("SendMessage", args: <Object>["", "", room,  true, false]);
     
+    receiveAllMessages(room);
+    _hubConnection.invoke("SendMessage", args: <Object>[username, "", room,  true, false]);
+    notifyPropertyChanged(chatMessagesPropName);
   }
 
-  void leave() async{
+  void leave(String username) async{
     _hubConnection.invoke("SendMessage", args: <Object>["", "", "", false, true]);
-    _chatMessages.clear();
     
     if(_room == "general") {
       return;
     }
 
     _room = "general";
-    join(_room);
+    join(_room, username);
     notifyPropertyChanged(roomPropName);
     notifyPropertyChanged(chatMessagesPropName);
   }
   
-
   void receiveMessage(List<Object>? args) {
-    final String senderName = args![0].toString();
+    final String user = args![0].toString();
     final String message = args[1].toString();
-    _chatMessages.add(ChatMessage(senderName, message));
+     _chatMessages.add(ChatMessage.recent(user, message));
     notifyPropertyChanged(chatMessagesPropName);
   }
 
-  void receiveAllMessages(_room) async{
+  void receiveAllMessages(String room) async{
+    
     try {
-        http.Response response = await http.get(
-            Uri.parse(consts.CHAT_ALL_MESSAGES + _room),
-            headers: {"Accept": "application/json"});
-
-        if (response.statusCode == 200) {
-          print('Returned with HTTP status: ${response.statusCode}');
-          String responseApi = response.body.toString().replaceAll("\n", "");
-          print(responseApi);
-        } else {
-          print('Returned with HTTP status: ${response.statusCode}');
-          String responseApi = response.body.toString().replaceAll("\n", "");
-
-          if (responseApi.isNotEmpty) {
-            
-          } else {
-            print('No content to show');
-          }
+      http.Response response = await http.get(
+        Uri.parse(consts.CHAT_ALL_MESSAGES + room),
+        headers: {"Accept": "application/json"}
+      );
+      if (response.statusCode == 200) {
+        print('Returned with HTTP status: ${response.statusCode} : SUCCESS');
+        List jsonData =
+        json.decode(response.body) as List<dynamic>;
+        print("Messages on the list : ${jsonData.length}");
+        //print(jsonData[1].runtimeType);
+        
+        for( var i = 0; i < jsonData.length; i++ ) { 
+          _chatMessages.add(ChatMessage.fromJson(jsonData[i]));
         }
-      } catch (e) {
-        print(e.toString());
+        
+        notifyPropertyChanged(chatMessagesPropName);
+      } else {
+        print('Returned with HTTP status: ${response.statusCode}');
       }
+    } catch (e) {
+      print("error");
+      print(e.toString());
+    }
   }
 }
 
@@ -172,8 +177,43 @@ class ChatPageViewModelProvider extends ViewModelProvider<ChatPageViewModel> {
 
   static ChatPageViewModel of(BuildContext context) {
     return (context
-            .dependOnInheritedWidgetOfExactType<ChatPageViewModelProvider>())!
-        .viewModel;
+      .dependOnInheritedWidgetOfExactType<ChatPageViewModelProvider>()
+    )!
+    .viewModel;
   }
+}
+
+class ChatMessage {
+
+  // Properites
+
+  late int id;
+  late String message;
+  late String user;
+  late String chatRoom;
+  late String timeStamp;
+  
+
+  // Methods
+
+  ChatMessage(
+    this.id, this.message, this.user,
+    this.chatRoom, this.timeStamp
+  );
+
+  ChatMessage.recent(this.user, this.message);
+
+  factory ChatMessage.fromJson(Map<String, dynamic> data) {
+  // note the explicit cast to String
+  // this is required if robust lint rules are enabled
+  final id = data['id'];
+  final message = data['message'];
+  final user = data['user'];
+  final chatRoom = data['chatRoom'];
+  final timeStamp = data['timestamp'];
+  return ChatMessage(id, message, user, chatRoom, timeStamp);
+}
+
+
 }
 
