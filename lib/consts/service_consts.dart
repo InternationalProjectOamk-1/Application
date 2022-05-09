f71ffe0cc444d367f56c10a74723321a9a0b5fc2
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-05-09 18:06:21 +0200

    Fixed invalid port number.

--- lib/consts/service_consts.dart
+++ lib/consts/service_consts.dart
@@ -8,5 +8,5 @@ const String REGISTER = 'Login/Register';
 // Different url for chat service
 const String CHAT = 'http://office.pepr.com:25253/chat';
 
-const String CHAT_ALL_MESSAGES = 'office.pepr.com:5100/ChatHistory/Get/';
+const String CHAT_ALL_MESSAGES = 'http://office.pepr.com:25253/ChatHistory/Get/';
 
