7a62750e57894c62264be2dd2ba113df883a897d
Andy <pmduck@hotmail.com>
2022-05-09 14:01:42 +0300

    removed unnescessary widgets, made new create event screen with all widgets in it

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mapplication/data/events.dart';
+import 'package:mapplication/styles/home_style.dart';
+import 'package:mapplication/views/create_event_screen.dart';
 import 'package:mapplication/views/profile_screen.dart';
-import 'package:mapplication/widgets/create_event.dart';
 import 'package:mapplication/widgets/event_builder.dart';
 import 'package:mapplication/widgets/profile_button.dart';
 
@@ -10,28 +11,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //var size = MediaQuery.of(context).size;
-    return Stack(
-      children: <Widget>[
-        SafeArea(
-          child: ListView.builder(
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          ListView.builder(
             itemCount: event_data.length,
             itemBuilder: (context, index) {
               return eventBuilder(context, index);
             },
           ),
-        ),
-        const EventButton(),
-        ProfileButton(
-          icon: Icons.person,
-          press: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ProfileScreen(),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CreateEventScreen(),
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size.width * .2,
+                    vertical: 10,
+                  ),
+                  decoration: myProfileCustom,
+                  child: const Icon(
+                    Icons.event,
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+          ProfileButton(
+            icon: Icons.person,
+            press: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProfileScreen(),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
