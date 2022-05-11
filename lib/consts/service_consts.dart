d2ffc1c93968953bc8a17516e4c8f094fa620d6c
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-05-11 19:14:27 +0200

    Correct Paths

--- lib/consts/service_consts.dart
+++ lib/consts/service_consts.dart
@@ -1,8 +1,11 @@
-const String BASE_PATH = 'office.pepr.com:25252';
+const String BASE_PATH = 'http://office.pepr.com:25252/';
+const String BASE_PATH2 = 'office.pepr.com:25252';
 
 const String LOGIN= '/Login';
 
 const String REGISTER = 'Login/Register';
+
+const String GET_USER_INFO = 'User/Details';
 
 
 // Different url for chat service
